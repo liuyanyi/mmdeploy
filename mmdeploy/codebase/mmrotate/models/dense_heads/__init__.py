886bfede6c2c162802ed710035575cf1432ca702
liuyanyi <wolfsonliu@163.com>
2022-11-22 11:04:57 +0800

    Add rewriter

--- mmdeploy/codebase/mmrotate/models/dense_heads/__init__.py
+++ mmdeploy/codebase/mmrotate/models/dense_heads/__init__.py
@@ -1,9 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .oriented_rpn_head import oriented_rpn_head__get_bboxes
-from .rotated_anchor_head import rotated_anchor_head__get_bbox
-from .rotated_rpn_head import rotated_rpn_head__get_bboxes
+# from .oriented_rpn_head import oriented_rpn_head__get_bboxes
+# from .rotated_anchor_head import rotated_anchor_head__get_bbox
+# from .rotated_rpn_head import rotated_rpn_head__get_bboxes
+from .rotated_rtmdet_head import rtmdet_head__predict_by_feat
 
 __all__ = [
-    'oriented_rpn_head__get_bboxes', 'rotated_anchor_head__get_bbox',
-    'rotated_rpn_head__get_bboxes'
+    # 'oriented_rpn_head__get_bboxes', 'rotated_anchor_head__get_bbox',
+    # 'rotated_rpn_head__get_bboxes'
+    'rtmdet_head__predict_by_feat'
 ]
